fe08174a1b459f76bf361cd93b063ddc35e35d86
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-07-25 13:34:48 +0300

    logout hook

--- src/shared/lib/hooks/useLogout.ts
+++ src/shared/lib/hooks/useLogout.ts
@@ -1,16 +1,16 @@
+"use client"
 import { useCallback } from "react";
 
-import { useCookie } from "./useCookie";
+import { setCookie } from "./useCookie";
 import { INITIAL_USER, useAuth } from "@/app/(auth)/auth-wrapper";
 import { useRouter } from "next/navigation";
 
 export const useLogout = () => {
-    const { removeCookie } = useCookie();
     const { setUser, setIsAuth } = useAuth();
     const router = useRouter();
 
     const logout = useCallback(() => {
-        removeCookie("access_token");
+        setCookie('access_token', "", 0)
         router.replace("/login");
         setIsAuth(false);
         setUser(INITIAL_USER);
