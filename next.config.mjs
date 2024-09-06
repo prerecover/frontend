4bb803aec77de034110dad6156d1788726092acf
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-09-06 19:08:21 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,8 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   images: { domains: ['localhost', "img-cdn.pixlr.com", "encrypted-tbn0.gstatic.com", "static.vecteezy.com", "www.fotor.com"] },
-
   env: {
-    BASE_URL: process.env.BACKEND
+    NEXT_PUBLIC_BACKEND: process.env.NEXT_PUBLIC_BACKEND
   },
 
 };
