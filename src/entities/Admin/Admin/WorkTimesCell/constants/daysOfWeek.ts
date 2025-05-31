33cf9dc7215ced005936ed3295e613def85b42ea
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-05-31 18:49:13 +0300

    UPDATE

---
+++ src/entities/Admin/Admin/WorkTimesCell/constants/daysOfWeek.ts
@@ -0,0 +1,9 @@
+const DAYS_OF_WEEK = [
+  { key: 'Monday', label: 'Понедельник' },
+  { key: 'Tuesday', label: 'Вторник' },
+  { key: 'Wednesday', label: 'Среда' },
+  { key: 'Thursday', label: 'Четверг' },
+  { key: 'Friday', label: 'Пятница' },
+  { key: 'Saturday', label: 'Суббота' },
+  { key: 'Sunday', label: 'Воскресенье' },
+] as const;
--- src/entities/Admin/Admin/WorkTimesCell/ui/Edit/index.tsx
+++ src/entities/Admin/Admin/WorkTimesCell/ui/Edit/index.tsx
@@ -8,21 +8,13 @@ import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { View } from '../View';
 import { TimeRangeInputs } from '../TimeRangeInputs';
 import { useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
+import { DAYS_OF_WEEK } from '../../constants/DaysOfWeek';
 
 interface Props<T extends EnTableTypes>
   extends TCellDataUpdate<T, TWorkTimeEdit> {
   data: TWorkTimeEdit;
 }
-
-const daysOfWeek = [
-  { key: 'Monday', label: 'Понедельник' },
-  { key: 'Tuesday', label: 'Вторник' },
-  { key: 'Wednesday', label: 'Среда' },
-  { key: 'Thursday', label: 'Четверг' },
-  { key: 'Friday', label: 'Пятница' },
-  { key: 'Saturday', label: 'Суббота' },
-  { key: 'Sunday', label: 'Воскресенье' },
-] as const;
 
 export const Edit = <T extends EnTableTypes>({
   data,
@@ -31,6 +23,9 @@ export const Edit = <T extends EnTableTypes>({
   id,
 }: Props<T>) => {
   const [workTime, setWorkTime] = useState<TWorkTimeEdit>(data);
+  const [visibleDays, setVisibleDays] = useState<Record<string, boolean>>(
+    Object.fromEntries(DAYS_OF_WEEK.map(({ key }) => [key, false]))
+  );
 
   const debounceUpdate = useDebounce((values: TWorkTimeEdit) => {
     updateFunc({ cellIndex, data: values, id });
@@ -48,24 +43,56 @@ export const Edit = <T extends EnTableTypes>({
       }));
     };
 
+  const toggleDay = (dayKey: string) => {
+    setVisibleDays((prev) => {
+      const newVisibleState = !prev[dayKey];
+
+      if (newVisibleState === false) {
+        // Если скрываем — удаляем время
+        setWorkTime((prevData) => ({
+          ...prevData,
+          [dayKey]: null,
+        }));
+      }
+
+      return {
+        ...prev,
+        [dayKey]: newVisibleState,
+      };
+    });
+  };
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
+      <DialogTrigger>
         <View data={data} />
       </DialogTrigger>
       <DialogContent
         hideCloseButton
-        className="py-6 px-5 max-w-[545px] overflow-visible rounded-xl shadow-[0_4px_10px_0_rgba(0,0,0,10%)] border-none"
+        className="py-12 px-14 rounded-[40px] max-w-none w-[590px] overflow-visible shadow-[0_4px_10px_0_rgba(0,0,0,10%)] border-none"
       >
         <ul className="space-y-4">
-          {daysOfWeek.map(({ key, label }) => (
-            <li key={key} className="flex gap-x-7">
-              <p>{label}</p>
-              <TimeRangeInputs
-                valueFrom={workTime[key] !== null ? workTime[key][0] : null}
-                valueTo={workTime[key] !== null ? workTime[key][1] : null}
-                onChange={handleTimeChange(key)}
-              />
+          {DAYS_OF_WEEK.map(({ key, label }) => (
+            <li key={key} className="flex justify-between">
+              <p
+                className={cn(
+                  'text-2xl px-4 rounded-2xl flex items-center justify-center cursor-pointer h-[45px] w-[205px] bg-blue-100 text-grey-600 duration-100',
+                  {
+                    ['bg-blue font-medium text-white']: visibleDays[key],
+                  }
+                )}
+                onClick={() => toggleDay(key)}
+              >
+                {label}
+              </p>
+
+              {visibleDays[key] ? (
+                <TimeRangeInputs
+                  valueFrom={workTime[key] !== null ? workTime[key][0] : null}
+                  valueTo={workTime[key] !== null ? workTime[key][1] : null}
+                  onChange={handleTimeChange(key)}
+                />
+              ) : null}
             </li>
           ))}
         </ul>
