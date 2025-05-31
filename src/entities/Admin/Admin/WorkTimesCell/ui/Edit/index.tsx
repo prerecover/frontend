4c22c1e24504ec953e84b0aad3c6cfba98e16b41
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-05-31 18:51:57 +0300

    UPDATE

--- src/entities/Admin/Admin/WorkTimesCell/ui/Edit/index.tsx
+++ src/entities/Admin/Admin/WorkTimesCell/ui/Edit/index.tsx
@@ -9,7 +9,16 @@ import { View } from '../View';
 import { TimeRangeInputs } from '../TimeRangeInputs';
 import { useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
-import { DAYS_OF_WEEK } from '../../constants/DaysOfWeek';
+
+const daysOfWeek = [
+  { key: 'Monday', label: 'Понедельник' },
+  { key: 'Tuesday', label: 'Вторник' },
+  { key: 'Wednesday', label: 'Среда' },
+  { key: 'Thursday', label: 'Четверг' },
+  { key: 'Friday', label: 'Пятница' },
+  { key: 'Saturday', label: 'Суббота' },
+  { key: 'Sunday', label: 'Воскресенье' },
+] as const;
 
 interface Props<T extends EnTableTypes>
   extends TCellDataUpdate<T, TWorkTimeEdit> {
@@ -24,7 +33,7 @@ export const Edit = <T extends EnTableTypes>({
 }: Props<T>) => {
   const [workTime, setWorkTime] = useState<TWorkTimeEdit>(data);
   const [visibleDays, setVisibleDays] = useState<Record<string, boolean>>(
-    Object.fromEntries(DAYS_OF_WEEK.map(({ key }) => [key, false]))
+    Object.fromEntries(daysOfWeek.map(({ key }) => [key, false]))
   );
 
   const debounceUpdate = useDebounce((values: TWorkTimeEdit) => {
@@ -72,7 +81,7 @@ export const Edit = <T extends EnTableTypes>({
         className="py-12 px-14 rounded-[40px] max-w-none w-[590px] overflow-visible shadow-[0_4px_10px_0_rgba(0,0,0,10%)] border-none"
       >
         <ul className="space-y-4">
-          {DAYS_OF_WEEK.map(({ key, label }) => (
+          {daysOfWeek.map(({ key, label }) => (
             <li key={key} className="flex justify-between">
               <p
                 className={cn(
