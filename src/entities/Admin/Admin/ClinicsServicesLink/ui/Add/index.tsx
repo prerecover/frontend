6efb65b88d3d1f8a1b953c0e640829bb372cd0e1
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-06-01 21:02:20 +0300

    UPDATE

--- src/entities/Admin/Admin/ClinicsServicesLink/ui/Add/index.tsx
+++ src/entities/Admin/Admin/ClinicsServicesLink/ui/Add/index.tsx
@@ -1,16 +1,15 @@
 'use client';
-import { TInlineAdd } from '@/shared/types/Admin/shared/Cells/Inline';
 import { TCellDataUpdate } from '@/shared/types/Admin/shared/Utils/CellDataUpdate';
 import { EnTableTypes } from '@/segments/Admin/MainTable';
 import { useDebounce } from '@/shared/hooks/useDebounce';
 import { useEffect } from 'react';
 import { ADMIN_ROUTES } from '@/shared/utils/paths';
-import { TLinkView } from '@/shared/types/Admin/shared/Cells/Link';
+import { TLinkAdd, TLinkView } from '@/shared/types/Admin/shared/Cells/Link';
 import { TData } from '..';
 
 interface Props<T extends EnTableTypes>
   extends TCellDataUpdate<T, TLinkView<TData>> {
-  data: TInlineAdd;
+  data: TLinkAdd;
 }
 
 const Add = <T extends EnTableTypes>({
